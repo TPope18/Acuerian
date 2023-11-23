d44ed0eefdbda732d92a37b1374ff576b41c26ae
Astroid45 <zayman45@gmail.com>
2023-11-23 00:19:42 -0500

    Gallery

--- src/activity.js
+++ src/activity.js
@@ -12,3 +12,55 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.about');
 hiddenElements.forEach((el) => observer.observe(el));
 
+var slideIndex = 0;
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+
+    // Hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = 0;
+        slides[i].style.display = "none";
+    }
+
+    // Increment slide index
+    slideIndex++;
+
+    // Reset index if it exceeds the number of slides
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    // Display the current slide with a fade-in effect
+    slides[slideIndex - 1].style.display = "block";
+    fadeIn(slides[slideIndex - 1], 2500); // Adjust the duration as needed
+
+    // Set timeout for the next slide
+    setTimeout(showSlides, 5000); // Change slide every 5 seconds (adjust as needed)
+}
+
+// Function to apply fade-in effect
+function fadeIn(element, duration) {
+    var opacity = 0;
+    var interval = 50; // Interval for opacity change
+    var delta = interval / duration;
+
+    // Function to update opacity
+    function increaseOpacity() {
+        opacity += delta;
+        element.style.opacity = opacity;
+
+        // Check if opacity reached 1 (fully visible)
+        if (opacity >= 1) {
+            clearInterval(fadeInterval);
+        }
+    }
+
+    // Set interval for opacity change
+    var fadeInterval = setInterval(increaseOpacity, interval);
+}
+
+// Start the auto-scrolling
+showSlides();
+
